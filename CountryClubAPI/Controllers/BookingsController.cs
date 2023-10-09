ffefc13353f03c82879c25bbb54e1023fdea66ec
jkimball54 <jeremyck54@gmail.com>
2023-10-09 14:31:58 -0600

    Creating a booking is implemented

--- CountryClubAPI/Controllers/BookingsController.cs
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -1,4 +1,5 @@
 ﻿using CountryClubAPI.DataAccess;
+using CountryClubAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CountryClubAPI.Controllers
@@ -18,6 +19,24 @@ namespace CountryClubAPI.Controllers
         {
             var bookings = _context.Bookings;
             return new JsonResult(bookings);
+        }
+
+        [HttpPost]
+        public ActionResult CreateBooking(Booking booking)
+        {
+            //Check if sent in model is valid
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+
+            var savedBooking = _context.Bookings.OrderBy(m => m.Id).Last();
+
+            Response.StatusCode = 201;
+            return new JsonResult(savedBooking);
         }
     }
 }
