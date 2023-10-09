d9d39d121de2d9defcf4b6b26c3b2983919fb1bd
jkimball54 <jeremyck54@gmail.com>
2023-10-09 13:20:45 -0600

    Setup BookingsController

---
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -0,0 +1,23 @@
+﻿using CountryClubAPI.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountryClubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : Controller
+    {
+        private readonly CountryClubContext _context;
+
+        public BookingsController(CountryClubContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult AllBookings()
+        {
+            var bookings = _context.Bookings;
+            return new JsonResult(bookings);
+        }
+    }
+}
